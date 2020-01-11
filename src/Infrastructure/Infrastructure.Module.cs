50ba3e7009d83bd1c5f760be68c18b646f04180a
Misha Tarnortusky <michael@sensory.co.il>
2020-01-11 11:11:08 -0500

    Codidact skeleton

---
+++ src/Infrastructure/Infrastructure.Module.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Codidact.Infrastructure
+{
+    public static class InfrastructureModule
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
