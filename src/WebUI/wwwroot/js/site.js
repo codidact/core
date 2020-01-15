5c531b7d75fc4209a61b925897de2b4ebb73b844
zoxuyu <maus@paulstrobach.de>
2020-01-15 22:57:56 +0100

    Add JS and fix HTML for header slides This includes the docs-Java Script for header slides (mobile navigation) and updates the nav HTML, by adding:
    - proper href attributes to links to provide full styling and full semantics, and
    - fixing the mobile GitHub link

--- src/WebUI/wwwroot/js/site.js
+++ src/WebUI/wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/*
+ * Header slides
+ ----------------
+ * This code powers the "header slides", which are at the core of mobile nav
+*/
 
-// Write your JavaScript code.
+const headerSlideTriggers = document.querySelectorAll("[data-trigger-header-slide]");
+
+for (let i = 0; i < headerSlideTriggers.length; i++) {
+    headerSlideTriggers[i].addEventListener("click", function (e) {
+        const headerSlide = document.querySelector(this.getAttribute("data-trigger-header-slide"));
+
+        headerSlide.classList.toggle("is-active");
+        this.classList.toggle("is-active");
+
+        // Position header slide appropriately relative to
+        // trigger.
+        const rect = this.getBoundingClientRect();
+        hs.style.top = (rect.top + rect.height) + "px";
+        hs.style.right = (document.body.clientWidth - rect.right) + "px";
+
+        // Prevent navigation
+        e.preventDefault();
+    });
+}
