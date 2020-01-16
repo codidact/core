c659d0c64f4c90c25008636027e827d830ea1f32
misha130 <misha130@gmail.com>
2020-01-16 08:50:24 -0500

    Fix one line if in string extensions

--- src/Domain/Extensions/StringExtensions.cs
+++ src/Domain/Extensions/StringExtensions.cs
@@ -11,7 +11,9 @@ namespace Codidact.Domain.Extensions
         /// <returns></returns>
         public static string ToSnakeCase(this string input)
         {
-            if (string.IsNullOrEmpty(input)) { return input; }
+            if (string.IsNullOrEmpty(input)) { 
+                return input;
+            }
 
             var startUnderscores = Regex.Match(input, @"^_+");
             return startUnderscores + Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2").ToLower();
