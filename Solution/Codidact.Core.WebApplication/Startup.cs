28a2bc0f88d33d84ee2e848e9af8d03a2972b017
Marc Ranolfi <mranolfi@gmail.com>
2020-01-17 08:17:38 -0300

    (refactor) insert space around nested parenthesis ("Find in Solution")

--- Solution/Codidact.Core.WebApplication/Startup.cs
+++ Solution/Codidact.Core.WebApplication/Startup.cs
@@ -21,7 +21,7 @@ namespace Codidact.Core.WebApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if ( env.IsDevelopment() )
             {
                 app.UseDeveloperExceptionPage();
             }
