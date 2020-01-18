8fb45d7fb1766097e17d2e449445fdf33d6e7272
misha130 <misha130@gmail.com>
2020-01-18 10:53:36 -0500

    Add MVC integration tests

---
+++ tests/WebUI.IntegrationTests/Controllers/Home/HomePageTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codidact.WebUI.IntegrationTests.Controllers.Home
+{
+    public class HomePageTests :
+      IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public HomePageTests(
+            CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/home")]
+        [InlineData("/home/index")]
+        [InlineData("/home/privacy")]
+        public async Task GetEndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData("/invalid-url")]
+        public async Task EnsureInvalidUrlsRedirectTo404(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
