3513ca8d733d5bb62afdd892dea70cbc5fb497a4
misha130 <misha130@gmail.com>
2020-01-27 20:55:57 -0500

    Change Auth site.js to es5

--- src/Auth/wwwroot/js/site.js
+++ src/Auth/wwwroot/js/site.js
@@ -4,18 +4,18 @@
  * This code powers the "header slides", which are at the core of mobile nav
 */
 
-const headerSlideTriggers = document.querySelectorAll("[data-trigger-header-slide]");
+var headerSlideTriggers = document.querySelectorAll("[data-trigger-header-slide]");
 
-for (let i = 0; i < headerSlideTriggers.length; i++) {
+for (var i = 0; i < headerSlideTriggers.length; i++) {
     headerSlideTriggers[i].addEventListener("click", function (e) {
-        const headerSlide = document.querySelector(this.getAttribute("data-trigger-header-slide"));
+        var headerSlide = document.querySelector(this.getAttribute("data-trigger-header-slide"));
 
         headerSlide.classList.toggle("is-active");
         this.classList.toggle("is-active");
 
         // Position header slide appropriately relative to
         // trigger.
-        const rect = this.getBoundingClientRect();
+        var rect = this.getBoundingClientRect();
         hs.style.top = (rect.top + rect.height) + "px";
         hs.style.right = (document.body.clientWidth - rect.right) + "px";
 
