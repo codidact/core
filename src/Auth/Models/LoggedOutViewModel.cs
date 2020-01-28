d620e9d3fb0187a0f54ac5850448502049c9ef6f
misha130 <misha130@gmail.com>
2020-01-27 21:08:39 -0500

    Remove unnecessary auth parts

--- src/Auth/Models/LoggedOutViewModel.cs
+++ src/Auth/Models/LoggedOutViewModel.cs
@@ -6,13 +6,15 @@ namespace Codidact.Auth.Models
     public class LoggedOutViewModel
     {
         public string PostLogoutRedirectUri { get; set; }
+        
         public string ClientName { get; set; }
-        public string SignOutIframeUrl { get; set; }
 
         public bool AutomaticRedirectAfterSignOut { get; set; } = false;
 
         public string LogoutId { get; set; }
+     
         public bool TriggerExternalSignout => ExternalAuthenticationScheme != null;
+        
         public string ExternalAuthenticationScheme { get; set; }
     }
 }
--- src/Auth/Models/LoginInputModel.cs
+++ src/Auth/Models/LoginInputModel.cs
@@ -9,9 +9,12 @@ namespace Codidact.Auth.Models
     {
         [Required]
         public string Username { get; set; }
+
         [Required]
         public string Password { get; set; }
+        
         public bool RememberLogin { get; set; }
+        
         public string ReturnUrl { get; set; }
     }
 }
