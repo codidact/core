88865a9440a000a02a0dc7acb380c8bf07dac49c
Misha Tarnortusky <michael@sensory.co.il>
2020-01-30 14:12:15 -0500

    Add TODO for CurrentCommunityService Test

---
+++ tests/WebUI.IntegrationTests/Services/CurrentCommunityServiceTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codidact.WebUI.IntegrationTests.Services
+{
+    public class CurrentCommunityServiceTests
+    {
+        //TODO: Test CurrentCommunityService with a custom IActionContextAccessor
+    }
+}
