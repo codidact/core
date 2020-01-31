47a8d5afa059b638fda17e4b75acedaa9e17ec0d
Misha Tarnortusky <michael@sensory.co.il>
2020-01-31 08:30:35 -0500

    Add class comment to CommunityException

--- src/Domain/Exceptions/CommunityException.cs
+++ src/Domain/Exceptions/CommunityException.cs
@@ -2,6 +2,10 @@
 
 namespace Codidact.Domain.Exceptions
 {
+    /// <summary>
+    /// Exception for invalid community, such as community does not exist
+    /// or community data problem.
+    /// </summary>
     public class CommunityInvalidException : Exception
     {
         public CommunityInvalidException(string message)
