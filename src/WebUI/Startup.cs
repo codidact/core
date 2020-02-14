81f8afff1e357409352fb740e43036b8c4f11b84
Misha Tarnortusky <michael@sensory.co.il>
2020-02-14 11:51:42 -0500

    Switch from Controllers to Razor Pages

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -56,7 +56,7 @@ namespace Codidact.WebUI
             JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
 
             services
-                .AddControllersWithViews()
+                .AddRazorPages()
                 .AddRazorRuntimeCompilation();
         }
 
@@ -70,10 +70,11 @@ namespace Codidact.WebUI
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
-            app.UseHsts();
+
             app.UseHttpsRedirection();
 
             app.UseStaticFiles();
@@ -83,12 +84,7 @@ namespace Codidact.WebUI
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseEndpoints(endpoints => endpoints.MapRazorPages());
 
             if (env.EnvironmentName != "Test")
             {
