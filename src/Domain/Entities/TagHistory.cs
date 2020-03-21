7db110886e377e18dfd84bab927792e09e2826f4
Misha Tarnortusky <michael@sensory.co.il>
2020-03-21 09:34:48 -0400

    Fix TagHistory Typos and Redudancy

--- src/Domain/Entities/TagHistory.cs
+++ src/Domain/Entities/TagHistory.cs
@@ -6,14 +6,7 @@ namespace Codidact.Core.Domain.Entities
 {
     public partial class TagHistory : AuditEntity<Tag>
     {
-        public long HistoryId { get; set; }
-        public DateTime HistoryActivityAt { get; set; }
-        public long HistoryMemberId { get; set; }
         public long Id { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime LastModifiedAt { get; set; }
-        public long CreatedByMemberId { get; set; }
-        public long LastModifiedByMemberId { get; set; }
         public string Body { get; set; }
         public string Description { get; set; }
         public string TagWiki { get; set; }
