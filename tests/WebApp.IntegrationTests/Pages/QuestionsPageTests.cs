d46a6ed857b45a35c252275c7a41c92643562c09
Misha Tarnortusky <michael@sensory.co.il>
2020-04-07 21:08:12 -0400

    Add Integration test for question

---
+++ tests/WebApp.IntegrationTests/Pages/QuestionsPageTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codidact.Core.WebApp.IntegrationTests.Pages
+{
+    public class QuestionsPageTests :
+      IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public QuestionsPageTests(
+            CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/questions")]
+        [InlineData("/questions?skip=0&take=20")]
+        public async Task ReturnsQuestionsByQueryParams(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
